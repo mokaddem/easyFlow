5e780c4d6501d94b229237ec1b8eaafa160d1f0a
Sami Mokaddem <mokaddem.sami@gmail.com>
2017-12-20 15:59:24 +0100

    feature: addition of buffer when linking 2 nodes

--- server.py
+++ server.py
@@ -9,14 +9,21 @@ import os
 
 app = Flask(__name__)
 
+def read_module_svg_template(filename):
+    with open('static/css/img/{}.svg'.format(filename), 'r') as f:
+        raw_svg = f.read()
+    raw_svg = raw_svg.replace("-inkscape-font-specification:'sans-serif Bold'", '') #removed bad options
+    raw_svg = "".join(raw_svg.splitlines())
+    return raw_svg
+
 @app.route("/")
 def index():
-    with open('static/css/img/module_templatev2.svg', 'r') as f:
-        raw_svg = f.read()
-    raw_svg = "".join(raw_svg.splitlines())
+    raw_module_svg = read_module_svg_template('module_templatev3')
+    raw_buffer_svg = read_module_svg_template('buffer_template')
 
     return render_template('index.html',
-                raw_svg=raw_svg,
+                raw_module_svg=raw_module_svg,
+                raw_buffer_svg=raw_buffer_svg
             )
 
 if __name__ == '__main__':

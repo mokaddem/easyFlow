e9130280254093fa81792fb0b183f72b3bfa915a
Sami Mokaddem <mokaddem.sami@gmail.com>
2018-01-10 10:29:45 +0100

    fix: forgotten scripts...

---
+++ link_manager.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3.5
+# -*-coding:UTF-8 -*
+
+from abc import ABCMeta, abstractmethod
+from time import sleep
+# import link
+import os, time, sys
+import redis, json
+
+from util import genUUID, objToDictionnary
+from alerts_manager import Alert_manager
+from process_metadata_interface import Process_metadata_interface
+
+class Link_manager:
+    def __init__(self, projectUUID, puuid):
+        # from puuid, get buuid
+        self.projectUUID = projectUUID
+        self.puuid = puuid
+        self._serv = redis.Redis(unix_socket_path='/tmp/redis.sock', decode_responses=True)
+        # ONLY 1 ingress and 1 egress
+        self.ingress = None
+        self.egress = None
+        self.update_connections()
+        self.partOfTheFlow = False
+
+    def update_connections(self):
+        self.ingress = None
+        self.egress = None
+        config = self.get_config()
+
+        for buuid, bConfig in config.items():
+            procTo = bConfig['toUUID']
+            procFrom = bConfig['fromUUID']
+            if procTo == self.puuid:
+                self.ingress = buuid
+            if procFrom == self.puuid:
+                self.egress = buuid
+        # self.show_connections()
+
+    def get_flowItem(self):
+        if self.ingress is not None:
+            flowItem = self._serv.rpop(self.ingress)
+            return flowItem
+        else:
+            # Either return None or wait until part of the flow
+            return None
+
+    def push_flowItem(self, flowItem):
+        if self.egress is not None:
+            self._serv.lpush(self.egress, flowItem)
+
+    def get_config(self):
+        return json.loads(self._serv.get(self.projectUUID))['buffers']
+
+    def show_connections(self):
+        print('{} -> {} -> {}'.format(self.ingress, self.puuid, self.egress))
+
+
+
+class Multiple_link_manager(Link_manager):
+    def __init__(self, projectUUID, puuid, custom_config, multi_in=True):
+        super().__init__(projectUUID, puuid)
+        self.custom_config = custom_config
+        self.multi_in = multi_in
+        self.multi_out = not multi_in
+        self.ingress = []
+        self.egress = []
+        print('MULTIPLE LINK MANAGER FOR '+puuid)
+
+        self.update_connections()
+        self.mult_logic = self.custom_config['multiplex_logic']
+        self.interleave_index = 0 # last poped buffer index
+
+    def inc_interleave_index(self):
+        self.interleave_index = self.interleave_index+1 if self.interleave_index < len(self.ingress)-1 else 0
+
+    def update_connections(self):
+        self.ingress = []
+        self.egress = []
+        config = self.get_config()
+
+        for buuid, bConfig in config.items():
+            procTo = bConfig['toUUID']
+            procFrom = bConfig['fromUUID']
+            if procTo == self.puuid:
+                self.ingress.append(buuid)
+            if procFrom == self.puuid:
+                self.egress.append(buuid)
+
+    def get_flowItem(self):
+        if self.multi_in: # custom logic: interleaving, priority
+            if self.mult_logic == 'Interleave':
+                # print('popping from '+self.ingress[self.interleave_index])
+                flowItem = self._serv.rpop(self.ingress[self.interleave_index])
+                self.inc_interleave_index()
+            elif self.mult_logic == 'Priority':
+                print('ingoring priority for the moment')
+                flowItem = self._serv.rpop(self.ingress[self.interleave_index])
+                self.inc_interleave_index()
+            else:
+                print('Unkown multiplexer logic')
+            return flowItem
+
+        else: # same as simple link manager
+            flowItem = self._serv.rpop(self.ingress[0])
+            return flowItem
+
+    def push_flowItem(self, flowItem):
+        if self.multi_out: # custom logic: copy, dispatch
+            if self.mult_logic == 'Interleave':
+                self._serv.lpush(self.egress[self.interleave_index], flowItem)
+                self.inc_interleave_index()
+            elif self.mult_logic == 'Priority':
+                print('ingoring priority for the moment')
+                self._serv.lpush(self.egress[self.interleave_index], flowItem)
+                self.inc_interleave_index()
+            else:
+                print('Unkown multiplexer logic')
+        else: # same as simple link manager
+            self._serv.lpush(self.egress[0], flowItem)
+
+
+# class Multiple_link_manager:
+#     def __init__(self, projectUUID, puuid, custom_config, multi_in=True):
+#         # from puuid, get buuid
+#         self.projectUUID = projectUUID
+#         self.puuid = puuid
+#         self.custom_config = custom_config
+#         self._serv = redis.Redis(unix_socket_path='/tmp/redis.sock', decode_responses=True)
+#         self.multi_in = multi_in
+#         self.multi_out = not multi_in
+#         self.ingress = []
+#         self.egress = []
+#         self.update_connections()
+#
+#         self.mult_logic = self.custom_config['multiplex_logic']
+#
+#     def update_connections(self):
+#         self.ingress = []
+#         self.egress = []
+#         config = self.get_config()
+#
+#         for buuid, bConfig in config.items():
+#             procTo = bConfig['toUUID']
+#             procFrom = bConfig['fromUUID']
+#             if procTo == self.puuid:
+#                 self.ingress.append(buuid)
+#             if procFrom == self.puuid:
+#                 self.egress.append(buuid)
+#
+#     def get_flowItem(self):
+#         if self.multi_in: # custom logic: interleaving, priority
+#             flowItem = self._serv.rpop(self.ingress)
+#             return flowItem
+#         else: # same as simple link manager
+#             flowItem = self._serv.rpop(self.ingress)
+#             return flowItem
+#
+#     def push_flowItem(self, flowItem):
+#         if self.multi_out: # custom logic: copy, dispatch
+#             self._serv.lpush(self.egress, flowItem)
+#         else: # same as simple link manager
+#             self._serv.lpush(self.egress, flowItem)
+#
+#     def get_config(self):
+#         return json.loads(self._serv.get(self.projectUUID))['buffers']
---
+++ process_no_input.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3.5
+# -*-coding:UTF-8 -*
+import sys, os
+curdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(curdir, '..'))
+
+import time
+import signal
+from abc import ABCMeta, abstractmethod
+from process import Process
+
+
+class Process_no_input(Process, metaclass=ABCMeta):
+    def __init__(self, *args, **kwargs):
+        signal.signal(signal.SIGUSR1, self.sig_handler)
+        super().__init__(*args, **kwargs)
+
+    def sig_handler(self, signum, frame):
+        self.process_commands()
+
+    def process_message(self, msg):
+        pass
+
+    @abstractmethod
+    def generate_data(self):
+        pass
+
+    def run(self):
+        self.push_process_start()
+        while True:
+            # No commands in generators modules
+            self.push_p_info()
+            self.generate_data()
+
+            time.sleep(0.1)
+            # print('process {} [{}]: sleeping'.format(self.puuid, self.pid))
+
+    # forward is called from generate_data().
+    def forward(self, msg):
+        self.push_p_info()
+        self._link_manager.push_flowItem(msg)
